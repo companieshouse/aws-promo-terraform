c9a7f7b5deb140afc3a45775529cb322dfdc475a
Will Watters <wwatters@companieshouse.gov.uk>
2021-09-03 16:51:37 +0100

    feature/cm-774-s3-promo-web-hosting

--- groups/infrastructure/locals.tf
+++ groups/infrastructure/locals.tf
@@ -2,7 +2,7 @@
 # Locals
 # ------------------------------------------------------------------------
 locals {
-  s3_promo_web_hosting_bucket = "${var.aws_account}.${var.aws_region}.resources.ch.gov.uk"
+  s3_promo_web_hosting_bucket = "${var.aws_account}.${var.aws_region}.promo-resources.ch.gov.uk"
 
   default_tags = {
     Terraform   = "true"
