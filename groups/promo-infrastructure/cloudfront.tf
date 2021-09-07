347707014d4d0636486296075d3b2545a324e56c
Will Watters <wwatters@companieshouse.gov.uk>
2021-09-07 21:48:34 +0100

    Promo S3 Bucket for S3 web hosting and Cloudfront

--- groups/promo-infrastructure/cloudfront.tf
+++ groups/promo-infrastructure/cloudfront.tf
@@ -1,10 +1,11 @@
+/*
 module "cloudfront_promo" {
   source  = "terraform-aws-modules/cloudfront/aws"
   version = "2.7.0"
 
   comment             = "Promo Site CloudFront"
   enabled             = true
-  is_ipv6_enabled     = true
+  is_ipv6_enabled     = false
   price_class         = "PriceClass_All"
   retain_on_delete    = false
   wait_for_deployment = false
@@ -29,5 +30,59 @@ module "cloudfront_promo" {
     cached_methods  = ["GET", "HEAD"]
     compress        = true
     query_string    = true
+  }
+}
+*/
+
+locals {
+  s3_origin_id = "myS3Origin"
+}
+
+resource "aws_cloudfront_distribution" "s3_distribution" {
+  origin {
+    domain_name = module.s3_promo_web_hosting_bucket.s3_bucket_website_endpoint
+    origin_id   = local.s3_origin_id
+  }
+
+  enabled             = true
+  is_ipv6_enabled     = true
+  comment             = "Some comment"
+  default_root_object = "index.html"
+
+  default_cache_behavior {
+    allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = local.s3_origin_id
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    #viewer_protocol_policy = "allow-all"
+    viewer_protocol_policy = "redirect-to-https"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+  }
+
+  price_class = "PriceClass_200"
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  tags = {
+    Environment = "production"
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+    minimum_protocol_version       = "TLSv1.2_2019"
   }
 }
