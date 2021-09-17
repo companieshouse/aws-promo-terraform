238bf85eba1649617da7965ecec044c1b28c91ff
Will Watters <wwatters@companieshouse.gov.uk>
2021-09-17 20:54:19 +0100

    Promo S3 Bucket for S3 web hosting and Cloudfront

--- groups/promo-infrastructure/s3-website-hosting.tf
+++ groups/promo-infrastructure/s3-website-hosting.tf
@@ -3,7 +3,7 @@ module "s3_promo_web_hosting_bucket" {
   version = "2.1.0"
 
   bucket = local.s3_promo_web_hosting_bucket
-  #acl    = "private"
+  acl    = "private"
 
   block_public_acls       = false
   block_public_policy     = false
@@ -41,6 +41,7 @@ module "s3_promo_web_hosting_bucket" {
     error_document = "error.html"
   }
 
+  /*
   acl = null # Grant conflicts with default of `acl = "private"` so set to null to use grants
   grant = [{
     type        = "Group"
@@ -48,6 +49,7 @@ module "s3_promo_web_hosting_bucket" {
     uri         = "http://acs.amazonaws.com/groups/s3/LogDelivery"
     }
   ]
+*/
 
   logging = {
     target_bucket = local.s3_promo_logs_bucket
